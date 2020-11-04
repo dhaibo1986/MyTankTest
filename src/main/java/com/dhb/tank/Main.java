194febfff6d73515f3c2ea79bbfc1de8173da8c4
haibo.duan <dhaibo1986@sohu.com>
2020-11-04 21:15:13 +0800

    坦克坐标设置为变量 paint改变让坦克动起来

---
+++ src/main/java/com/dhb/tank/Main.java
@@ -0,0 +1,8 @@
+package com.dhb.tank;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TankFrame tf = new TankFrame();
+	}
+}
---
+++ src/main/java/com/dhb/tank/TankFrame.java
@@ -0,0 +1,32 @@
+package com.dhb.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+	int x = 200,y=200;
+
+	public TankFrame() {
+		setSize(800,600);
+		setResizable(false);
+		setTitle("tank war");
+		setVisible(true);
+
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		System.out.println("paint");
+		g.fillRect(x,y,50,50);
+		x+=10;
+		y+=10;
+	}
+}
