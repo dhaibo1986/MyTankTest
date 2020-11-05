e4166c7f9b4567e51f0919c2e8a1698bcee256b0
haibo.duan <dhaibo1986@sohu.com>
2020-11-05 19:40:43 +0800

    根据上下左右键的状态判断坦克的前进方向。主要用于处理同时按键的时候的问题，对于计算机处理同时按键有先后。

--- src/main/java/com/dhb/tank/TankFrame.java
+++ src/main/java/com/dhb/tank/TankFrame.java
@@ -8,10 +8,10 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
-	int x = 200,y=200;
+	int x = 200, y = 200;
 
 	public TankFrame() {
-		setSize(800,600);
+		setSize(800, 600);
 		setResizable(false);
 		setTitle("tank war");
 		setVisible(true);
@@ -28,23 +28,59 @@ public class TankFrame extends Frame {
 
 	@Override
 	public void paint(Graphics g) {
-		System.out.println("paint");
-		g.fillRect(x,y,50,50);
+//		System.out.println("paint");
+		g.fillRect(x, y, 50, 50);
 //		x+=10;
 //		y+=10;
 	}
 
 	class MyKeyListener extends KeyAdapter {
 
+		boolean BL = false;
+		boolean BR = false;
+		boolean BU = false;
+		boolean BD = false;
+
 		@Override
 		public void keyPressed(KeyEvent e) {
-			System.out.println("key pressed");
-			x += 30;
+			int key = e.getExtendedKeyCode();
+			switch (key) {
+				case KeyEvent.VK_LEFT:
+					BL = true;
+					break;
+				case KeyEvent.VK_UP:
+					BU = true;
+					break;
+				case KeyEvent.VK_DOWN:
+					BD = true;
+					break;
+				case KeyEvent.VK_RIGHT:
+					BR = true;
+					break;
+				default:
+					break;
+			}
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			System.out.println("key released");
+			int key = e.getExtendedKeyCode();
+			switch (key) {
+				case KeyEvent.VK_LEFT:
+					BL = false;
+					break;
+				case KeyEvent.VK_UP:
+					BU = false;
+					break;
+				case KeyEvent.VK_DOWN:
+					BD = false;
+					break;
+				case KeyEvent.VK_RIGHT:
+					BR = false;
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
