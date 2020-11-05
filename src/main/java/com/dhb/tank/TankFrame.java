3ef972d12c78c30811b71dfc376296f273e57301
haibo.duan <dhaibo1986@sohu.com>
2020-11-05 19:15:35 +0800

    添加键盘监听事件

--- src/main/java/com/dhb/tank/TankFrame.java
+++ src/main/java/com/dhb/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.dhb.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -20,6 +22,8 @@ public class TankFrame extends Frame {
 				System.exit(0);
 			}
 		});
+
+		this.addKeyListener(new MyKeyListener());
 	}
 
 	@Override
@@ -28,5 +32,18 @@ public class TankFrame extends Frame {
 		g.fillRect(x,y,50,50);
 		x+=10;
 		y+=10;
+	}
+
+	class MyKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("key pressed");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("key released");
+		}
 	}
 }
