3fed8d6d17065dfa0d12770914c101840dd65b25
haibo.duan <dhaibo1986@sohu.com>
2020-11-08 18:39:32 +0800

    new出5个敌方坦克

--- src/main/java/com/dhb/tank/Main.java
+++ src/main/java/com/dhb/tank/Main.java
@@ -1,11 +1,18 @@
 package com.dhb.tank;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class Main {
 
 	public static void main(String[] args) {
 		TankFrame tf = new TankFrame();
+
+		//初始化敌方坦克
+		for(int i=0;i<5;i++) {
+			tf.tanks.add(new Tank(50+i*80,200,Dir.DOWN,tf));
+		}
+
 		while (true) {
 			try {
 				TimeUnit.MILLISECONDS.sleep(50);
--- src/main/java/com/dhb/tank/TankFrame.java
+++ src/main/java/com/dhb/tank/TankFrame.java
@@ -11,8 +11,9 @@ import java.util.List;
 public class TankFrame extends Frame {
 
 	public static final int SPEED = 10;
-	Tank myTank = new Tank(200, 200, Dir.DOWN,this);
+	Tank myTank = new Tank(200, 400, Dir.DOWN,this);
 	List<Bullet> bullets = new ArrayList<>();
+	List<Tank>  tanks = new ArrayList<>();
 	Bullet b = new Bullet(300,300,Dir.DOWN,this);
 
 	public static final int GAME_WIDTH = 800;
@@ -59,6 +60,9 @@ public class TankFrame extends Frame {
 		g.drawString("子弹的数量："+bullets.size(),10,60);
 		g.setColor(c);
 		myTank.paint(g);
+		for(int i=0;i<tanks.size();i++) {
+			tanks.get(i).paint(g);
+		}
 		for(int i=0;i< bullets.size();i++){
 			bullets.get(i).paint(g);
 		}
