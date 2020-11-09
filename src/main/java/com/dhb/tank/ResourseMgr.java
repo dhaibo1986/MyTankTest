da276a8ff4c620f41f548a843aac0eef7c65547d
haibo.duan <dhaibo1986@sohu.com>
2020-11-09 19:59:04 +0800

    加入声音文件，对图片进行优化，采用一张图片的旋转来实现，替换原来的4张图片。

--- src/main/java/com/dhb/tank/ResourseMgr.java
+++ src/main/java/com/dhb/tank/ResourseMgr.java
@@ -14,15 +14,15 @@ public class ResourseMgr {
 
 	static {
 		try {
-			tankL = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\tankL.gif"));
-			tankU = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\tankU.gif"));
-			tankR = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\tankR.gif"));
-			tankD = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\tankD.gif"));
+			tankU = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\BadTank1.png"));
+			tankL = ImageUtil.rotateImage(tankU,-90);
+			tankR = ImageUtil.rotateImage(tankU,90);
+			tankD = ImageUtil.rotateImage(tankU,180);
 
-			bulletL = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\bulletL.gif"));
-			bulletU = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\bulletU.gif"));
-			bulletR = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\bulletR.gif"));
-			bulletD = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\bulletD.gif"));
+			bulletU = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\bulletU.png"));
+			bulletL = ImageUtil.rotateImage(bulletU,-90);
+			bulletR = ImageUtil.rotateImage(bulletU,90);
+			bulletD = ImageUtil.rotateImage(bulletU,180);
 
 			for(int i=0;i<16;i++) {
 				explodes[i] = ImageIO.read(ResourseMgr.class.getClassLoader().getResourceAsStream("images\\e"+(i+1)+".gif"));
--- src/main/java/com/dhb/tank/TankFrame.java
+++ src/main/java/com/dhb/tank/TankFrame.java
@@ -18,8 +18,8 @@ public class TankFrame extends Frame {
 	Bullet b = new Bullet(300,300,Dir.DOWN,Group.GOOD,this);
 
 
-	public static final int GAME_WIDTH = 800;
-	public static final int GAME_HEIGHT = 600;
+	public static final int GAME_WIDTH = 1080;
+	public static final int GAME_HEIGHT = 960;
 
 
 
