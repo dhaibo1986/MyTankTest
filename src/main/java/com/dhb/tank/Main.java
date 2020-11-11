4f2583905edcfff5965185ff92cb5a74f228fe92
haibo.duan <dhaibo1986@sohu.com>
2020-11-11 20:04:37 +0800

    增加声音  使用Audio工具类

--- src/main/java/com/dhb/tank/Main.java
+++ src/main/java/com/dhb/tank/Main.java
@@ -14,6 +14,7 @@ public class Main {
 			tf.tanks.add(new Tank(50+i*80,200,Dir.DOWN,Group.BAD,tf));
 		}
 
+		new Thread(()->new Audio("audio/war1.wav").loop()).start();
 		while (true) {
 			try {
 				TimeUnit.MILLISECONDS.sleep(50);
